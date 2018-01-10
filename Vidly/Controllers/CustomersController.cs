e48575af563f22c619033b4c6e36db0c20270207
shafikshaon <shafikshaon@gmail.com>
2018-01-10 22:33:53 +0600

    Add new customer form template added

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -26,6 +27,16 @@ namespace Vidly.Controllers
         {
             var customers = _context.Customers.Include(c => c.MembershipType);
             return View(customers);
+        }
+
+        public ActionResult New()
+        {
+            var membershipTypes = _context.MembershipTypes.ToList();
+            var viewModel = new NewCustomerViewModel
+            {
+                MembershipTypes = membershipTypes
+            };
+            return View(viewModel);
         }
 
         public ActionResult Details(int id)
