0414223a510251695e662d44076481bf12872002
shafikshaon <shafikshaon@gmail.com>
2018-01-11 20:18:51 +0600

    Movie form client and server side validation added

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -9,6 +9,7 @@ namespace Vidly.Models
     public class Movie
     {
         public int Id { get; set; }
+
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
@@ -25,6 +26,7 @@ namespace Vidly.Models
         public DateTime ReleaseDate { get; set; }
 
         [Display(Name = "Number in Stock")]
+        [Range(1, 20)]
         public byte NumberInStock { get; set; }
     }
 }
